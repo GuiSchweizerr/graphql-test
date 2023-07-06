7aed42441247e9fc031f0fbb64859397f5ada5db
Guilherme Schweizer <guilherme.schweizer@dentsu.com>
2023-07-06 17:19:17 -0300

    wip

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,7 @@
+export class User {
+    id: string;
+    name: string;
+    role: string;
+    created_at: Date;
+    updated_at: Date;
+}
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,37 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Resolver('User')
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Mutation('createUser')
+  create(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.usersService.create(createUserInput);
+  }
+
+  @Query('users')
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Query('user')
+  findOne(@Args('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Mutation('updateUser')
+  update(@Args('updateUserInput') id: string, name: string, email: string) {
+    return this.usersService.update({
+      where: { id: Number(id) },
+      data: { name, email },
+    });
+  }
+
+  @Mutation('removeUser')
+  remove(@Args('id') id: number) {
+    return this.usersService.remove({ id });
+  }
+}
